e07076608234af6ef7dba816b580cea343d33660
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-11 23:44:05 +0200

    intento 4

--- prueba.py
+++ prueba.py
@@ -5,17 +5,17 @@ class Calcular:
         
         sympy.Eq(y(x).diff(x), f)
 
-    @property   
+    
     def getter(self):
         # Resolviendo la ecuación
-        solucion = sympy.dsolve(y(x).diff(x) - f)
+        solucion = sympy.dsolve(f)
         return solucion
 
 x = sympy.symbols('x')
 y = sympy.Function('y')
 
 # Defino la función
-f = ((y(x))*x**2 - (y(x))/((y(x)) + 1))
+f = y(x).diff(x) - (y(x)*x**2 - y(x))/(y(x) + 1)
 calcular = Calcular(x, y, f)
 solucion = calcular.getter()
 print(solucion)
