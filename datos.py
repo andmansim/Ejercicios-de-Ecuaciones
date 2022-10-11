936c74a0093f6ae3c017e8caad05a93f9b25b693
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-11 23:07:04 +0200

    solution

--- datos.py
+++ datos.py
@@ -9,15 +9,15 @@ class Calcular:
         ec = sympy.Eq(self.y(self.x).diff(self.x), self.f)
 
         # Resolviendo la ecuación
-        solucion = sympy.dsolve(y(x).diff(x) - f)
+        solucion = sympy.dsolve(self.y(self.x).diff(self.x) - self.f)
 
     def condicion_inicial(self):
     
         # Condición inicial
-        ics = {y(0): 2}
+        ics = {self.y(0): 2}
 
         #sustituimos la condición inicial
-        C_eq = sympy.Eq(solucion.lhs.subs(x, 0).subs(ics), solucion.rhs.subs(x, 0))
+        C_eq = sympy.Eq(solucion.lhs.subs(self.x, 0).subs(ics), solucion.rhs.subs(self.x, 0))
 
         #Nos saca la C
         c = sympy.solve(C_eq)[0] #Con [0] pq nos lo da en una lista
