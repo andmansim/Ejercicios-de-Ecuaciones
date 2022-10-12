b38e5a2c76a3d9366315fe18ebe673d73019e574
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-12 20:54:57 +0200

    intento 4

--- prueba.py
+++ prueba.py
@@ -10,7 +10,24 @@ class Calcular:
     def getter(self):
         # Resolviendo la ecuación
         solucion = sympy.dsolve(f)
-        return solucion[1]
+        return solucion
+    
+    def condicion_inicial(self, c):
+     
+        
+        C_eq = sympy.Eq(solucion.lhs.subs(x, 0).subs(ics), solucion.rhs.subs(x, 0))
+
+
+        print(C_eq)
+
+        #Nos saca la C
+        ci = sympy.solve(C_eq)[0] #Con [0] pq nos lo da en una lista
+        print(ci)
+
+        #Solución particular
+
+        solucion_p = sympy.Eq(y(x), c* sympy.exp(-x**3) + 2)
+        return solucion_p
 
 x = sympy.symbols('x')
 y = sympy.Function('y')
@@ -19,4 +36,7 @@ y = sympy.Function('y')
 f = y(x).diff(x)  - (y(x) *sympy.log(y(x)))/sympy.sin(x)
 calcular = Calcular(x, y, f)
 solucion = calcular.getter()
+ics = {y(sympy.pi/2): sympy.exp}
+c = calcular.condicion_inicial(ics)
+print(c)
 print(solucion)
